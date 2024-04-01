baa3f05d15776b9de58b8b3f2ab1e1664acede37
Slavikb1 <93644262+Slavikb1@users.noreply.github.com>
2024-04-01 17:11:06 +0300

    Update instanse.tf

--- terraform/instanse.tf
+++ terraform/instanse.tf
@@ -1,1 +1,13 @@
+resource "aws_instance" "pomapp" {
+  ami               = var.AMIS[var.REGION]
+  instance_type     = "t2.micro"
+  availability_zone = var.ZONE1
+  key_name = "kubernetes"
+  vpc_security_group_ids = ["sg-0b36d0afa317cfb8f"]
+  vpc_security_group_ids = ["${aws_security_group.terraform_allow.id}"]
 
+
+  tags = {
+    Name = "pom deploy"
+  }
+}
