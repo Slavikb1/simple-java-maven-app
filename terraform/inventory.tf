88fcf6dfb735a5c9402f477503f2cc6b31401322
Slavikb1 <93644262+Slavikb1@users.noreply.github.com>
2024-04-01 16:58:37 +0300

    Update inventory.tf

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,1 +1,9 @@
+resource "aws_instance" "pomapp" {
+  ami               = var.AMIS[var.REGION]
+  instance_type     = "t2.micro"
+  availability_zone = var.ZONE1
+  key_name = "kubernetes"
 
+  tags = {
+    Name = "pom deploy"
+  }
