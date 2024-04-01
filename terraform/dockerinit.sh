01afbaac3151b62e4d343394779eac106b87de73
Slavikb1 <93644262+Slavikb1@users.noreply.github.com>
2024-04-01 18:27:59 +0300

    Update dockerinit.sh

--- terraform/dockerinit.sh
+++ terraform/dockerinit.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo yum update
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum update -y
+sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker $USER
+sudo usermod -a -G docker $(whoami)
+newgrp docker
 
 sudo ducker run -d slavikb/maven_project:latest
