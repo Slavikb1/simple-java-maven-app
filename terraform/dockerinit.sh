983c72ab7f7fd7b7d1506e0d5dc04c3e699bf77c
Slavikb1 <93644262+Slavikb1@users.noreply.github.com>
2024-04-01 18:00:46 +0300

    Update dockerinit.sh

--- terraform/dockerinit.sh
+++ terraform/dockerinit.sh
@@ -4,7 +4,8 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce         
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ubuntu 
 sudo usermod -aG docker $USER
 
 sudo ducker run slavikb/maven_project:latest
