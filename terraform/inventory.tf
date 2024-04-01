2944a1ab22f2e4a474813f5cab6766482cb5cc66
Slavikb1 <93644262+Slavikb1@users.noreply.github.com>
2024-04-01 17:09:43 +0300

    Update inventory.tf

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -3,7 +3,11 @@ resource "aws_instance" "pomapp" {
   instance_type     = "t2.micro"
   availability_zone = var.ZONE1
   key_name = "kubernetes"
+  vpc_security_group_ids = ["sg-0b36d0afa317cfb8f"]
+  vpc_security_group_ids = ["${aws_security_group.terraform_allow.id}"]
+
 
   tags = {
     Name = "pom deploy"
   }
+}
